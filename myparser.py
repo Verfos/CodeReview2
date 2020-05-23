a08834a8988bf69a3cacdd2c6fdc1445f685b16e
Verfos <senya-vanin@rambler.ru>
2020-05-23 15:44:57 +0300

    Third

--- myparser.py
+++
@@ -1,5 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='Testing App')
-parser.add_argument('port', type=int, default = 5000)
-parser.add_argument('host', type=str, nargs='?',default = 'localhost')
